9ca4e7386154716cc13443b91e6d0ebc6a87b1a6
nicolefemello <nicfmello@gmail.com>
2025-02-19 00:14:21 -0300

    FEAT: update home page and add products page

---
+++ src/app/produtos/page.tsx
@@ -0,0 +1,7 @@
+const ProductsPage = () => {
+    return ( <div className="p-5 border-red-500 rounded-xl">
+                <h1 className="text-red-500">Products Page</h1>
+            </div> );
+}
+ 
+export default ProductsPage;
