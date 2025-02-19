8145bd546aef14cb3867aec49727b0d918f63057
nicolefemello <nicfmello@gmail.com>
2025-02-19 17:12:02 -0300

    FEAT: add shadcn initial setup and components

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const ProductsPage = () => {
-    return ( <div className="p-5 border-red-500 rounded-xl">
-                <h1 className="text-red-500">Products Page</h1>
-            </div> );
+    return ( <div>
+        <h1>Products Page</h1>
+        <Button>MUITOS PRODUTOS</Button>
+        <p>nu, muitos mesmo!!</p>
+        <Input />
+    </div>
+);
 }
  
 export default ProductsPage;
