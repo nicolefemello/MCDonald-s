f54092eaec1bfc7d233878935468fbbb90e64f10
nicolefemello <nicfmello@gmail.com>
2025-02-21 23:45:20 -0300

    FEAT: ensure cart product can be removed

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -14,7 +14,8 @@ export interface ICartContext {
     toggleCart: () => void,
     addProduct: (product: CartProduct) => void,
     decreaseProductQuantity: (productId: string) => void,
-    increaseProductQuantity: (productId: string) => void
+    increaseProductQuantity: (productId: string) => void,
+    removeProduct: (productId: string) => void
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -23,7 +24,8 @@ export const CartContext = createContext<ICartContext>({
     toggleCart: () => {},
     addProduct: () => {},
     decreaseProductQuantity: () => {},
-    increaseProductQuantity: () => {}
+    increaseProductQuantity: () => {},
+    removeProduct: () => {}
 })
 
 export const CartProvider = ({children}: {children: ReactNode}) => {
@@ -65,6 +67,9 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
             })
         })
     }
+    const removeProduct = (productId: string) => {
+        setProducts(prevProducts => prevProducts.filter(prevProduct => prevProduct.id != productId));
+    }
     return (
         <CartContext.Provider value={{
             isOpen,
@@ -72,7 +77,8 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
             toggleCart,
             addProduct,
             decreaseProductQuantity,
-            increaseProductQuantity
+            increaseProductQuantity,
+            removeProduct
         }}>
             {children}
         </CartContext.Provider>
